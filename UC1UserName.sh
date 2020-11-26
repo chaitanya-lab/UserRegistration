29ad594f2e989ae34282a05a178a2d2b26beede4
unknown <you@example.com>
2020-11-26 08:47:16 +0530

    Added file UC1UserName

--- UC1UserName.sh
+++ UC1UserName.sh
@@ -1,1 +1,13 @@
-demo is hereO
+#!/bin/bash -x
+shopt -s extglob
+read -p "Enter the Email  :" firstname
+pat='^[a-zA-Z]{3,}$'
+if [[ $firstname =~ $pat ]] #+
+then
+        echo "user name is valid"
+else
+        echo " user name not valid"
+fi
+
+
+
